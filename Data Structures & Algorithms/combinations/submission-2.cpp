fae4fc4b2b74bde5cc7b327d7c72dae9324412d3
Akhilesh7-irl <akhileshpachpawar@gmail.com>
2026-07-14 13:11:41 +0530

    Add: combinations - submission-2

---
+++ Data Structures & Algorithms/combinations/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    
+    
+    void backtrack(int n , int k ,vector<int>& cur, int start){
+        if(cur.size() == k){
+            res.push_back(cur);
+            return;
+        }
+        for(int i = start ; i < n ; i++){
+            cur.push_back(i+1);
+            backtrack(n , k ,cur , i+1);
+            cur.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        
+        vector<int> cur;
+        backtrack( n , k , cur , 0);
+        
+        return res;
+        
+    }
+};
